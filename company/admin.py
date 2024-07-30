53ecca7cdddffce420d87c11598536faf7a1ef15
Jihyun-Choi <034179@naver.com>
2024-07-30 19:04:15 +0900

    feat: user, company admin 추가

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Company
 
-# Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "company_name",
+        "country",
+        "region",
+    )
+    search_fields = (
+        "company_name",
+        "country",
+        "region",
+    )
+    list_filter = (
+        "country",
+        "region",
+    )
+    ordering = ("company_name",)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from user.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Admin View for User"""
+
+    list_display = (
+        "email",
+        "user_name",
+    )
