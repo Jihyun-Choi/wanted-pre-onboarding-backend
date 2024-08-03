1e49cd3b1f24ba35cf5931bc32f40079285218d4
Jihyun-Choi <034179@naver.com>
2024-08-03 18:16:44 +0900

    feat: job_posting 상세 페이지 선택사항 구현
    - JobPostingDetailSerializer에 other_job_postings field 추가
    - other_job_postings: 해당 회사가 올린 다른 채용공고의 id 리스트

--- recruitment/serializers/job_posting_detail_serializer.py
+++ recruitment/serializers/job_posting_detail_serializer.py
@@ -1,9 +1,12 @@
+from rest_framework import serializers
 from recruitment.models import JobPosting
 from recruitment.serializers import JobPostingSerializer
 
 
 class JobPostingDetailSerializer(JobPostingSerializer):
     """Serializer definition for JobPosting Model."""
+
+    other_job_postings = serializers.SerializerMethodField()
 
     class Meta:
         """Meta definition for JobPostingDetailSerializer."""
@@ -18,10 +21,19 @@ class JobPostingDetailSerializer(JobPostingSerializer):
             "reward",
             "skills",
             "description",
+            "other_job_postings",
         ]
         read_only_fields = [
             "id",
             "company_name",
             "country",
             "region",
+            "other_job_postings",
         ]
+
+    def get_other_job_postings(self, obj):
+        return (
+            JobPosting.objects.filter(company=obj.company)
+            .exclude(id=obj.id)
+            .values_list("id", flat=True)
+        )
