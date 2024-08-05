33bd72f7af544c49d9713e1c66770de9c2cf4d82
Jihyun-Choi <034179@naver.com>
2024-08-05 15:44:15 +0900

    fix: user model 수정
    - email field에 unique 조건 추가

--- user/models.py
+++ user/models.py
@@ -4,11 +4,8 @@ from django.db import models
 class User(models.Model):
     """Model definition for User."""
 
-    USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["email"]
-
     user_name = models.CharField(max_length=255)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
 
     class Meta:
         """Meta definition for User."""
