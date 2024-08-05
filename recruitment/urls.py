6508ce4fdd88bcc6669ec19e95e3156e9d73a2e5
Jihyun-Choi <034179@naver.com>
2024-08-05 15:55:47 +0900

    fix: urls 경로 수정

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -1,15 +1,15 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from recruitment.views import JobPostingViewSet
-
+from recruitment.views import JobPostingViewSet, JobApplicationCreateAPIView
 
 app_name = "recruitment"
 router = DefaultRouter()
 router.register(r"", JobPostingViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),
     path(
         "apply/", JobApplicationCreateAPIView.as_view(), name="job-application-create"
     ),
 ]
+
+urlpatterns += router.urls
